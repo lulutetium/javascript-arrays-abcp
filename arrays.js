2dfeac88572008869b8b29a83b6c7445e1f70281
Zacharoula 'Lulu' Morfogen <lulu@cipher-schema.net>
2018-01-14 17:47:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,26 +1,23 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  array.push(element);
-  return array;
+ return[element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array = [element, ...array];
-  return array;
+ array.push(element);
+ return array
 }
 
 function addElementToEndOfArray(array, element){
-  array.pull(element);
-  return array;
+ return[...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  array = [...array, element];
-  return array;
+ array.pull(element);
+ return array;
 }
 
-function accessElementInArray(array, index){
-  return array[index];
+function accessElementInArray(array, index) {
+ return array[index];
 }
-
