30b9d2f1d3c21541a9ac99ccaf455ccb7268c410
Zacharoula 'Lulu' Morfogen <lulu@cipher-schema.net>
2018-01-14 18:15:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,4 +22,11 @@ function accessElementInArray(array, index) {
  return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array)
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  
+}
